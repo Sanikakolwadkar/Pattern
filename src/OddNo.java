f40920ece6702c056a78976b85e6428c1638d454
Sanikakolwadkar <sanikakolwadkar11@gmail.com>
2024-11-22 10:09:32 +0530

    first commit

---
+++ src/OddNo.java
@@ -0,0 +1,14 @@
+
+public class OddNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=30;i++) {
+			if(i%2==1) {
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
